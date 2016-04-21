a7c7488d9153de66da27f537b19b4cf616274eb7
mkfritz <mary.k.fritz@gmail.com>
2016-04-21 11:07:11 -0700

    Update main.js

--- main.js
+++ main.js
@@ -7,19 +7,43 @@
 
 //1. Log your name to the console (as a string).
 
-console.log("Question 1");
-console.log(yourAnswer);
+console.log('Question 1');
+console.log('Mary');
 
 //2. Declare a variable called 'name', and then log it to the console.
+
+console.log('Question 2');
+var name = 'Mary';
 
 //3. Declare two variables with number values. 
 //   Add, subtract, multiply and divide them.
 
+console.log('Question 3');
+var x = 3;
+var y = 5;
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+
 //4. Declare two variables with string values. 
 //   Create a third variable that concatenates them.
 
+console.log('Question 4');
+var subject = 'Prince';
+var reaction = 'NOOOOOOOO';
+console.log(subject  +  reaction);
+
 //5. Add, subract, multiply and divide the strings '5' and '3'. 
 //   Observe the results and know how to explain the results if asked.
+
+console.log('Question 5');
+var five = '5';
+var three = '3';
+console.log(five + three);
+console.log(five - three);
+console.log(five * three);
+console.log(five / three);
 
 //6. Create a prompt that asks a user to pick a number. 
 //   Then double the answer.
